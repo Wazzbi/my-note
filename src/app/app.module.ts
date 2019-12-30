69964cc265867abe89a611e283bac18c751da118
David Novotny <davidnovotny47@gmail.com>
2019-12-30 17:39:26 +0100

    trochu css + button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,21 +4,18 @@ import { NgModule } from "@angular/core";
 import { AppComponent } from "./app.component";
 import { HomeComponent } from "./home/home.component";
 import { MainPanelComponent } from "./main-panel/main-panel.component";
-import { TestComponent } from './test/test.component';
+import { MatButtonModule, MatIconModule } from "@angular/material";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 @NgModule({
-   declarations: [
-      AppComponent,
-      HomeComponent,
-      MainPanelComponent,
-      TestComponent
-   ],
-   imports: [
-      BrowserModule
-   ],
-   providers: [],
-   bootstrap: [
-      AppComponent
-   ]
+  declarations: [AppComponent, HomeComponent, MainPanelComponent],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule {}
