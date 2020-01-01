59538f357262a4a4746232715ae338d67e7de9d9
David Novotny <davidnovotny47@gmail.com>
2020-01-01 13:39:16 +0100

    cudlik uz neco dela :D

--- src/app/main-panel/main-panel.component.ts
+++ src/app/main-panel/main-panel.component.ts
@@ -1,3 +1,4 @@
+import { element } from "protractor";
 import { MyServiceService } from "./../my-service.service";
 import { HomeComponent } from "./../home/home.component";
 import { Note } from "./../note";
@@ -18,9 +19,17 @@ export class MainPanelComponent implements OnInit {
   ngOnInit() {}
 
   saveText(): void {
-    this.note = this.input.getInput();
-    this.myServiceService.addNote(this.note).subscribe(note => {
+    let za = <HTMLTextAreaElement>document.getElementById("input");
+    let note = new Note();
+    note.data = za.value;
+    this.myServiceService.addNote(note).subscribe(note => {
       this.notes.push(note);
     });
+    this.clear();
+  }
+
+  clear(): void {
+    let ta = <HTMLTextAreaElement>document.getElementById("input");
+    ta.value = "";
   }
 }
